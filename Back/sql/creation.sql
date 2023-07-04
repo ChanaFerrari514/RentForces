277fafd596e7d8085a4738728ab6cc987ee11315
ChanaFerrari514 <charlyneferrari@gmail.com>
2023-07-04 20:49:59 +0200

    ...

--- Back/sql/creation.sql
+++ Back/sql/creation.sql
@@ -9,3 +9,24 @@ CREATE TABLE IF NOT EXISTS users (
     password TEXT NOT NULL
 );
 
+-- Accommodations
+
+CREATE TABLE IF NOT EXISTS Accommodations (
+    id INT PRIMARY KEY,
+    owner_id INT,
+    accommodation_name VARCHAR(100),
+    description VARCHAR(300),
+    location TEXT,
+    price DECIMAL,
+    availability BOOLEAN
+);
+
+-- Reservations
+
+CREATE TABLE IF NOT EXISTS Reservations (
+    reservation_id SERIAL PRIMARY KEY,
+    user_id UUID REFERENCES Users(id) ON DELETE CASCADE,
+    accommodation_id INT,
+    start_date DATE,
+    end_date DATE
+);
