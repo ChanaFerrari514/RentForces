891c6a13eac36babdc03b2ca88546b8713d8a49c
ChanaFerrari514 <charlyneferrari@gmail.com>
2023-07-17 20:17:17 +0200

    LegalNotice

--- Front/src/pages/LegalNotice.jsx
+++ Front/src/pages/LegalNotice.jsx
@@ -3,12 +3,12 @@ import NavbarBottom from '../components/Navbar/NavbarBottom';
 import { Link } from 'wouter';
 
 function LegalNotice() {
-    // Votre code pour la page LegalNotice ici
+  
     return (
       <div>
 
         <NavbarBottom />
-        {/* Votre contenu pour la page LegalNotice */}
+      
       </div>
     );
   }
